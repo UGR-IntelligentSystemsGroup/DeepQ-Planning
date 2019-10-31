e60ce12ce493bbe411630a758fe58d777a7c76c9
TheAeryan <carlosmarista@gmail.com>
2019-10-31 13:52:33 +0100

    Continued training

--- GVGAI/clients/GVGAI-PythonClient/src/MyAgent/Agent.py
+++ GVGAI/clients/GVGAI-PythonClient/src/MyAgent/Agent.py
@@ -43,7 +43,7 @@ class Agent(AbstractPlayer):
         self.EXECUTION_MODE="train" # Automatically changed by ejecutar_pruebas.py!
 
         # Name of the DQNetwork. Also used for creating the name of file to save and load the model from
-        self.network_name="DQN_alfa-0.005_dropout-0.4_batch-16_its-5000_24" # Automatically changed by ejecutar_pruebas.py!
+        self.network_name="DQN_alfa-0.005_dropout-0.4_batch-16_its-5000_28" # Automatically changed by ejecutar_pruebas.py!
 
         # Sizes of datasets to train the model on. For each size, a different model is created and trained in the training phase.
         self.datasets_sizes_for_training = [500, 1000, 2500, 5000, 7500, 10000]
